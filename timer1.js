2d22ecf42b4935ac1c783229b57d24da1a41d0ea
Kevin Holmes <102200682+sendd-k@users.noreply.github.com>
2022-05-10 16:41:43 -0700

    Update timer1.js

--- timer1.js
+++ timer1.js
@@ -1,6 +1,6 @@
 let time = process.argv.slice(2);
-time = time.filter(num => !isNaN(num))
-time = time.filter(num => num >= 0)
+time = time.filter(num => !isNaN(num));
+time = time.filter(num => num >= 0);
 
 time.forEach(time => setTimeout(() => {
   process.stdout.write('\x07');
